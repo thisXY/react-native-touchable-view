e72ba8e99b842a4138765e17b789e041f59631da
xianyu <xianyu@nibirutech.com>
2019-01-15 19:06:48 +0800

    publish

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # react-native-touchable-view
 你可以依赖这个TouchableView的长按,滑动,X轴滑动,Y轴滑动,长按后滑动等手势响应得到回调和一系列参数做你的业务。
+
+### 安装
+
+```bash
+npm install react-native-touchable-view --save
+```
+
+### 属性
+| parameter              | type                                 | required | default | description                                                                                                                                                                                                                        
+| :--------------------- | :----------------------------------- | :------- | :-------| :-------------------------------------------------------------------------------------------------                                                                 
+| style                  | oneOfType([number, object, array])   | no       |         | 样式    
+| longPressTestTime      | number                               | no       | 500     | 长按判定时间 (ms)
+| moveTestDistance       | number                               | no       | 10      | 滑动判定距离
+| moveCoolingTime        | number                               | no       | 10      | 滑动冷却时间 (ms)
+| onPressIn              | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onPressOut             | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onPress                | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onLongPress            | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onMove                 | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onLongMove             | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onMoveXStart           | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onMoveX                | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onMoveYStart           | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+| onMoveY                | func                                 | no       |         | 按下开始 (<br><br>evt: 可以在evt.nativeEvent里获取页面坐标,相对坐标等信息, <br><br>gestureState<br><br>)
+
+### 源码
+
+https://github.com/thisXY/react-native-touchable-view
---
+++ src/index.js
@@ -0,0 +1,219 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { View, PanResponder } from 'react-native';
+/**
+ * 手势响应
+ */
+class Touchable extends Component {
+  static propTypes = {
+    // 样式
+    style: PropTypes.oneOfType([PropTypes.number, PropTypes.object, PropTypes.array]),
+    // 子组件
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.arrayOf(PropTypes.element)]),
+    // 长按判定时间 (ms)
+    longPressTestTime: PropTypes.number,
+    // 滑动判定距离
+    moveTestDistance: PropTypes.number,
+    // 滑动冷却时间 (ms)
+    moveCoolingTime: PropTypes.number,
+    /**
+     * 按下开始
+     * @param evt
+     * @param gestureState
+     */
+    onPressIn: PropTypes.func,
+    /**
+     * 按下结束
+     * @param evt
+     * @param gestureState
+     */
+    onPressOut: PropTypes.func,
+    /**
+     * 按下
+     * @param evt
+     * @param gestureState
+     */
+    onPress: PropTypes.func,
+    /**
+     * 长按
+     * @param evt
+     * @param gestureState
+     */
+    onLongPress: PropTypes.func,
+    /**
+     * 滑动
+     * @param evt
+     * @param gestureState
+     */
+    onMove: PropTypes.func,
+    /**
+     * 长按滑动
+     * @param evt
+     * @param gestureState
+     */
+    onLongMove: PropTypes.func,
+    /**
+     * X轴滑动开始
+     * @param evt
+     * @param gestureState
+     */
+    onMoveXStart: PropTypes.func,
+    /**
+     * X轴滑动
+     * @param evt
+     * @param gestureState
+     */
+    onMoveX: PropTypes.func,
+    /**
+     * Y轴滑动开始
+     * @param evt
+     * @param gestureState
+     */
+    onMoveYStart: PropTypes.func,
+    /**
+     * Y轴滑动
+     * @param evt
+     * @param gestureState
+     */
+    onMoveY: PropTypes.func,
+  };
+
+  static defaultProps = {
+    style: null,
+    children: null,
+    longPressTestTime: 500,
+    moveTestDistance: 10,
+    moveCoolingTime: 10,
+    onPressIn: () => {},
+    onPressOut: () => {},
+    onPress: () => {},
+    onLongPress: () => {},
+    onMove: () => {},
+    onLongMove: () => {},
+    onMoveXStart: () => {},
+    onMoveX: () => {},
+    onMoveYStart: () => {},
+    onMoveY: () => {},
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+
+    // 按下坐标
+    this.pressInCoordinate = { x: 0, y: 0 };
+    // 长按判定定时器
+    this.longPressTestTimer = null;
+    // 滑动方向 ['X', 'Y', false]
+    this.moveDirection = false;
+    // 是否长按
+    this.isLongPress = false;
+    // 滑动时间记录
+    this.moveTime = 0;
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponderCapture: () => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        this.pressInCoordinate = { x: evt.nativeEvent.pageX, y: evt.nativeEvent.pageY };
+        this.moveDirection = false;
+        this.isLongPress = false;
+        const evtRecord = { ...evt };
+        const gestureStateRecord = { ...gestureState };
+
+        this.clearLongPressTestTimer();
+        this.longPressTestTimer = setTimeout(() => {
+          if (!this.moveDirection) {
+            // 长按
+            this.props.onLongPress(evtRecord, gestureStateRecord);
+            this.isLongPress = true;
+          }
+        }, this.props.longPressTestTime);
+
+        // 按下开始
+        this.props.onPressIn(evt, gestureState);
+      },
+
+      onPanResponderMove: (evt, gestureState) => {
+        const moveCoolingTime = this.moveTime;
+        this.moveTime = new Date().getTime();
+        if (this.moveTime - moveCoolingTime <= this.props.moveCoolingTime) {
+          return;
+        }
+
+        const coordinate = { x: evt.nativeEvent.pageX, y: evt.nativeEvent.pageY };
+        switch (this.moveDirection) {
+          // X轴滑动
+          case 'X':
+            this.props.onMoveX(evt, gestureState);
+            break;
+          // Y轴滑动
+          case 'Y':
+            this.props.onMoveY(evt, gestureState);
+            break;
+          // 未判定滑动方向
+          case false:
+            if ((coordinate.y - this.pressInCoordinate.y) ** 2 + (coordinate.x - this.pressInCoordinate.x) ** 2 >= this.props.moveTestDistance ** 2) {
+              const tan = (coordinate.y - this.pressInCoordinate.y) / (coordinate.x - this.pressInCoordinate.x);
+              if (tan >= 1 || tan <= -1) {
+                this.moveDirection = 'Y';
+                this.props.onMoveYStart(evt, gestureState);
+                this.props.onMoveY(evt, gestureState);
+              }
+              else {
+                this.moveDirection = 'X';
+                this.props.onMoveXStart(evt, gestureState);
+                this.props.onMoveX(evt, gestureState);
+              }
+            }
+            break;
+        }
+        // 滑动
+        this.props.onMove(evt, gestureState);
+        if (this.isLongPress) {
+          // 长按滑动
+          this.props.onLongMove(evt, gestureState);
+        }
+      },
+
+      onPanResponderRelease: (evt, gestureState) => {
+        this.onPressOut(evt, gestureState);
+      },
+
+      onPanResponderTerminate: (evt, gestureState) => {
+        this.onPressOut(evt, gestureState);
+      },
+    });
+  }
+
+  clearLongPressTestTimer = () => {
+    if (this.longPressTestTimer) {
+      clearTimeout(this.longPressTestTimer);
+      this.longPressTestTimer = null;
+    }
+  }
+
+  onPressOut = (evt, gestureState) => {
+    this.clearLongPressTestTimer();
+    // 无滑动
+    if (!this.moveDirection) {
+      // 按下
+      this.props.onPress(evt, gestureState);
+    }
+    // 按下结束
+    this.props.onPressOut(evt, gestureState);
+  }
+
+  render() {
+    return (
+      <View
+        style={this.props.style}
+        {...this.panResponder.panHandlers}
+      >
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+export default Touchable;
